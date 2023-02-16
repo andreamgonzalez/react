3541b4c6249865a5ed204af57686be5c42181a89
Andrea Michelle <andreagon6135@gmail.com>
2023-02-16 18:16:41 -0500

    Add files via upload

---
+++ eightball/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Eightball from "./Eightball";
+
+function App() {
+  return (
+    <div className="App">
+      <Eightball />
+      <button>Reset</button>
+    </div>
+  );
+}
+
+export default App;
